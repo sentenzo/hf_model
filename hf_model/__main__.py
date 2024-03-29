a06efa6f90b256012a39408978636df2d4ed7284
sentenzo <sentenzo@users.noreply.github.com>
2024-03-29 13:51:40 +0300

    `outlines` example

--- hf_model/__main__.py
+++ hf_model/__main__.py
@@ -1,1 +1,19 @@
 # https://huggingface.co/TheBloke/rocket-3B-GGUF
+
+import os
+
+import outlines
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = outlines.models.llamacpp(os.getenv("PATH_TO_MODEL", ""))
+prompt = """You are a sentiment-labelling assistant.
+Is the following review positive or negative?
+
+Review: This restaurant is just awesome!
+"""
+
+generator = outlines.generate.choice(model, ["Positive", "Negative"])
+answer = generator(prompt)
+print(answer)
