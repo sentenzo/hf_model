70a153ebbb1958d11f5082272d86514ed1d83962
sentenzo <sentenzo@users.noreply.github.com>
2024-03-29 14:53:03 +0300

    `outlines` improved example

--- hf_model/__main__.py
+++ hf_model/__main__.py
@@ -2,18 +2,47 @@
 
 import os
 
-import outlines
 from dotenv import load_dotenv
+from outlines.generate import choice
+from outlines.models import llamacpp
 
 load_dotenv()
 
-model = outlines.models.llamacpp(os.getenv("PATH_TO_MODEL", ""))
-prompt = """You are a sentiment-labelling assistant.
+model_path = os.getenv("PATH_TO_MODEL", "")
+model = llamacpp(
+    model_path,
+    model_kwargs={
+        "n_gpu_layers": -1,
+        "n_batch": 512,
+        "n_ctx": 1000,
+        "temp": 0.8,
+    },
+)
+prompt_stub = """You are a sentiment-labelling assistant.
 Is the following review positive or negative?
 
-Review: This restaurant is just awesome!
-"""
+Review: """
 
-generator = outlines.generate.choice(model, ["Positive", "Negative"])
-answer = generator(prompt)
-print(answer)
+inputs = [
+    # Positive
+    "This restaurant is just awesome!",
+    "Great service and a wonderful dining experience!",
+    "Highly recommend this place for its fantastic atmosphere!",
+    "The staff was friendly and attentive, making our visit enjoyable.",
+    "Amazing flavors and exceptional presentation, a top-notch restaurant!",
+    # Negative
+    "Terrible food quality and disappointing service.",
+    "The restaurant was unclean and poorly maintained.",
+    "Overpriced dishes that lacked flavor and creativity.",
+    "Rude staff members ruined the dining experience.",
+    "Long wait times and chaotic atmosphere, not worth a visit.",
+]
+
+
+generator = choice(model, ["Positive", "Negative"])
+answers = []
+for message in inputs:
+    answer = generator(prompt_stub + message)
+    answers.append(answer)
+for message, answer in zip(inputs, answers):
+    print(answer, "-", message)
